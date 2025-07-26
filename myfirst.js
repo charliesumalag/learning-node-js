1ae44614ec5f45839a4999cd266bea3b2345b039
Charlie Sumalag <charliesumalag444@yahoo.com.ph>
2025-07-26 15:23:42 +0800

    adding my first npm package module

--- myfirst.js
+++ myfirst.js
@@ -1,9 +1,8 @@
 let http = require('http');
-
+let uc = require('upper-case')
 http.createServer(function (req, res) {
     res.writeHead(200, {
         'Content-Type': 'text/html'
     });
-
-    res.end('Hello World!');
+    res.end(uc.upperCase('Hello World! this is my first node js server'));
 }).listen(8000);
